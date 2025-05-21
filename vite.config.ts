ba3e232fc5b953f13e493923f243db63a48297e5
Fireploy <fireployvm@gmail.com>
2025-05-21 12:23:19 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,22 @@
-import { defineConfig, loadEnv } from 'vite';
+import { ConfigEnv, defineConfig, loadEnv, UserConfigExport } from 'vite';
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default ({ mode }) => {
+export default ({ mode }: ConfigEnv): UserConfigExport  => {
   const env = loadEnv(mode, process.cwd());
-  console.log('VITE_BASE_PATH:', env.VITE_BASE_PATH);  // Verifica la carga
-  console.log('VITE_PORT:', env.VITE_PORT);  // Verifica la carga
+  //console.log('VITE_BASE_PATH:', env.VITE_BASE_PATH);  // Verifica la carga
+  //console.log('VITE_PORT:', env.VITE_PORT);  // Verifica la carga
 
   return defineConfig({
     base: `${env.VITE_BASE_PATH}`,
     plugins: [react()],
     server: {
       port: parseInt(env.VITE_PORT),
-      host: '0.0.0.0',
-      allowedHosts: ['proyectos.fireploy.online'],
+      host: true
     },
     preview: {
       port: parseInt(env.VITE_PORT),
       host: true,  // permite 0.0.0.0
-      allowedHosts: ['proyectos.fireploy.online'],
     },
   });
 };
